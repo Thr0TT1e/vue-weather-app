a7f4f3964e05b8c01ac9ca8d661b81a68ff9426a
Thr0TT1e <7hr0771e@gmail.com>
2022-01-04 23:27:25 +0300

    Реализовано получение и отображение погоды

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-# vue_weather_app
+# Vue.js weather app
+
+Данные по прогнозу погоды получены с [OpenWeather](https://openweathermap.org/current)
+
+* * *
+Результат работы можно посмотреть тут: <a href="https://vue-weather.noble24.pro/" target="_blank"> <img src="https://img.shields.io/badge/Site-Noble24.pro-orange?style=for-the-badge" alt="Страница на Noble24.pro"> </a> и <a href="https://vue-weather-noble24.netlify.app/" target="_blank"> <img src="https://img.shields.io/badge/Site-Nitlify.com-blue?style=for-the-badge" alt="Страница на Netlify.com"> </a>
+* * *
+
+[![wakatime](https://wakatime.com/badge/user/387a55ad-b952-4b4b-a858-04fb88e1e94c/project/226cdd24-0ec8-4887-90ae-3deba23c7a3c.svg?style=for-the-badge)](https://wakatime.com/badge/user/387a55ad-b952-4b4b-a858-04fb88e1e94c/project/226cdd24-0ec8-4887-90ae-3deba23c7a3c)
+
+| Холодно | Жарко |
+| ------- |:-----:|
+|![Скриншот страницы](./cold.png)|![Скриншот страницы](./warm.png)|
+
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,183 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+      id="app"
+      :class="typeof weather.main !== 'undefined' && weather.main.temp > 16 ? 'warm' : ''"
+  >
+    <main>
+      <div class="search-box">
+        <input
+            v-model="query"
+            @keypress="fetchWeather"
+            type="text"
+            class="search-bar"
+            placeholder="Поиск..."
+        >
+      </div>
+      
+      <div
+          class="weather-wrap"
+          v-if="typeof weather.main !== 'undefined'"
+      >
+        <div class="location-box">
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
+        </div>
+        
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }} °C</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  
+  data() {
+    return {
+      API_KEY:  '67d8a54513d0c00719d6884c4136ba51',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query:    '',
+      weather:  {},
+    }
+  },
+  
+  methods: {
+    fetchWeather(event) {
+      if (event.key === 'Enter') {
+        this.fetchToAPI()
+      }
+    },
+    
+    fetchToAPI() {
+      fetch(`${this.url_base}weather?q=${this.query ? this.query : 'Moscow'}&units=metric&appid=${this.API_KEY}`)
+          .then(res => res.json())
+          .then(this.setResults)
+          .catch(err => console.log(err))
+    },
+    
+    setResults(results) {
+      this.weather = results
+    },
+    
+    dateBuilder() {
+      let d      = new Date(),
+          months = ['Январь', 'Февраль', 'Март', 'Апрель',
+                    'Май', 'Июнь', 'Июль', 'Август',
+                    'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+          days   = ['Понедельник', 'Вторник', 'Среда', 'Четверг',
+                    'Пятница', 'Суббота', 'Воскресенье'],
+          day    = days[d.getDay()],
+          date   = d.getDate(),
+          month  = months[d.getMonth()],
+          year   = d.getFullYear();
+      
+      return `${day} ${date} ${month} ${year}`
+    },
+  },
+  
+  mounted() {
+    this.fetchToAPI()
+  },
 }
 </script>
 
 <style>
+* {
+  margin:     0;
+  padding:    0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Montserrat", sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  background-image:    url("./assets/cold.jpg");
+  background-size:     cover;
+  background-position: bottom;
+  transition:          .4s;
+}
+
+#app.warm {
+  background-image: url("./assets/warm.jpg");
+}
+
+main {
+  min-height:       100vh;
+  padding:          25px;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, .35), rgba(0, 0, 0, .75));
+}
+
+.search-box {
+  width:         100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display:          block;
+  width:            100%;
+  padding:          15px;
+  color:            #313131;
+  font-size:        20px;
+  appearance:       none;
+  border:           none;
+  outline:          none;
+  background:       none;
+  box-shadow:       0 0 8px rgba(0, 0, 0, .25);
+  background-color: rgba(255, 255, 255, .5);
+  border-radius:    0 16px 0 16px;
+  transition:       .4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow:       0 0 16px rgba(0, 0, 0, .25);
+  background-color: rgba(255, 255, 255, .75);
+  border-radius:    16px 0 16px 0;
+}
+
+.location-box .location {
+  color:       #fff;
+  font-size:   32px;
+  font-weight: 500;
+  text-align:  center;
+  text-shadow: 1px 3px rgba(0, 0, 0, .25);
+}
+
+.location-box .date {
+  color:       #fff;
+  font-size:   20px;
+  font-weight: 300;
+  font-style:  italic;
+  text-align:  center;
+}
+
+.weather-box {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.weather-box .temp {
+  display:          inline-block;
+  padding:          10px 25px;
+  color:            #ffffff;
+  font-size:        102px;
+  font-weight:      900;
+  text-shadow:      3px 6px rgba(0, 0, 0, .25);
+  background-color: rgba(255, 255, 255, .25);
+  border-radius:    16px;
+  margin:           30px 0;
+  box-shadow:       3px 6px rgba(0, 0, 0, .25);
+}
+
+.weather-box .weather {
+  color:       white;
+  font-size:   48px;
+  font-weight: 700;
+  font-style:  italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, .25);
 }
 </style>
